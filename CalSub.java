9c6b315d926c5fc154d88e08ffe7671b19b61d3e
keepgoingautumn <lms301301@naver.com>
2026-03-11 22:08:25 +0900

    ch02_pjt_02
    DI 방식으로 변경

---
+++ CalSub.java
@@ -0,0 +1,8 @@
+package ch02_pjt_01;
+
+public class CalSub  implements  ICalculator{
+    @Override
+    public int do0peration(int firstNum, int secondNum) {
+        return firstNum - secondNum;
+    }
+}
