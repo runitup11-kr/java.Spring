cf8e39c2d33692af282b52b95cdb0f9146657b56
keepgoingautumn <lms301301@naver.com>
2026-03-11 22:16:30 +0900

    Delete CalSub.java

--- CalSub.java
+++
@@ -1,8 +0,0 @@
-package ch02_pjt_01;
-
-public class CalSub  implements  ICalculator{
-    @Override
-    public int do0peration(int firstNum, int secondNum) {
-        return firstNum - secondNum;
-    }
-}
