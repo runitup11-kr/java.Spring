aa2d4a08ce30a80023753408cbc14b3971b3a345
keepgoingautumn <lms301301@naver.com>
2026-03-11 22:16:16 +0900

    Delete CalDiv.java

--- CalDiv.java
+++
@@ -1,8 +0,0 @@
-package ch02_pjt_01;
-
-public class CalDiv implements  ICalculator{
-    @Override
-    public int do0peration(int firstNum, int secondNum) {
-        return secondNum != 0 ? (firstNum / secondNum) : 0;
-    }
-}
